1ab002097e5b2aefc92c3c2a4e7443fb60be1539
shrayyef <m_shrayyef90@hotmail.com>
2021-12-17 00:00:33 +0300

    init

--- src/components/Price.js
+++ src/components/Price.js
@@ -16,12 +16,12 @@ export default ({old_price, new_price, sm, style}) => {
         style={{fontSize: sm ? 16 : 22, marginEnd: sm ? 0 : 10}}
         grey={new_price < old_price}
         lineThrough={new_price < old_price}>
-        {old_price * currency.value}
+        {(old_price * currency.value).toFixed(2)}
         {currency.sign}
       </Text>
       {new_price < old_price && (
         <Text h3={!sm} p={sm} primary>
-          {new_price * currency.value}
+          {(new_price * currency.value).toFixed(2)}
           {currency.sign}
         </Text>
       )}
